ac89a30857e5259ddb8b36af666cf6593cd73f03
MrHerbat <kocirel206@gmail.com>
2024-09-17 15:21:03 +0200

    added cw4 with tables

---
+++ cw4/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] tab = new int[10];
+
+for (int i = 0; i < tab.length(); i++)
+{
+    tab[i] - i*i;
+}
+
+ShowTable(tab);
+
+void ShowTable(int[] tab)
+{
+    foreach (int element in tab)
+    {
+        Console.Write(element + " ");
+    }
+}
