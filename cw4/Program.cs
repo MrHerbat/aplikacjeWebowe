88c6655f05a92a18dcbe47f2fa0362c58a8a9100
MrHerbat <kocirel206@gmail.com>
2024-09-17 15:59:10 +0200

    cw4 - added math functions for tabs

--- cw4/Program.cs
+++ cw4/Program.cs
@@ -8,10 +8,74 @@ for (int i = 0; i < tab.Length; i++)
 
 ShowTable(tabString);
 
+var tab2 = GenerTab(10);
+ShowTable(tab2);
+Console.WriteLine();
+Console.WriteLine(GetMax(tab2));
+Console.WriteLine(GetMin(tab2));
+Console.WriteLine(GetMax(tab2)-GetMin(tab2));
+Console.WriteLine(GetSum(tab2));
+Console.WriteLine(GetAvg(tab2));
+
+
+
+
 void ShowTable<T>(T[] tab)
 {
     foreach (T element in tab)
     {
         Console.Write(element + " ");
     }
+}
+
+int[] GenerTab(int size)
+{
+    Random rand = new Random();
+    int tabSize = size;
+    int[] result = new int[tabSize];
+    for (int i = 0; i < tabSize; i++)
+    {
+        result[i] = rand.Next(100);
+    }
+    return result;
+}
+int GetMax(int[] tab)
+{
+    int maxValue = 0;
+    foreach (var element in tab)
+    {
+        if (maxValue <= element)
+        {
+            maxValue = element;
+        }
+    }   
+    return maxValue;
+}
+int GetMin(int[] tab)
+{
+    int minValue = 100;
+    foreach (var element in tab)
+    {
+        if (minValue >= element)
+        {
+            minValue = element;
+        }
+    }   
+    return minValue;
+}
+int GetSum(int[] tab)
+{
+    int sum = 0;
+    foreach (var element in tab)
+    {
+        sum+=element;
+    }
+    return sum;
+}
+float GetAvg(int[] tab)
+{
+    int sum = GetSum(tab);
+    int size = tab.Length;
+    float avg = sum/size;
+    return avg;
 }
