524f77dd6bad9bbb218d9839dd9544ce738816b8
MrHerbat <kocirel206@gmail.com>
2024-09-24 15:37:53 +0200

    added html,css,js,person.cs

--- cw1/obj/Debug/net8.0/cw1.AssemblyInfo.cs
+++ cw1/obj/Debug/net8.0/cw1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cw1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dcc3eac8ac1a750cee2e8f8186ed16be6b4d1e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("cw1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cw1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- cw5/Program.cs
+++ cw5/Program.cs
@@ -1,6 +1,21 @@
+using cw5.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseStaticFiles();
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/strona1", () => "Strona 1");
+app.MapGet("/strona2", () => GetPeople());
 
 app.Run();
+
+
+List<Person> GetPeople()
+{
+    var list = new List<>();
+    list.Add(new Person { FirstName = "Jan", LastName = "Kowalski", Age = 20 });
+    list.Add(new Person { FirstName = "Damian", LastName = "Jabłko", Age = 14 });
+    list.Add(new Person { FirstName = "Nikolai", LastName = "Romanov", Age = 156 });
+    return list;
+}
