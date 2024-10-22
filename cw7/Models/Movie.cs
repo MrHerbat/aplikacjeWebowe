9e46ffd26de8225738dd8132232fd80fa23201e0
MrHerbat <kocirel206@gmail.com>
2024-10-22 16:12:56 +0200

    FIXED FRIKIN FINALLY

--- cw7/Models/Movie.cs
+++ cw7/Models/Movie.cs
@@ -1,26 +1,32 @@
+using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+
 
 namespace cw7.Models;
 
 public class Movie
 {
-    
     public int Id { get; set; }
+    
     [DisplayName("Tytuł")]
     [Required(ErrorMessage = "Tytuł jest wymagany")]
-    public string? Title { get; set; }
+    public string Title { get; set; }
+    
     [DisplayName("Reżyser")]
     [Required(ErrorMessage = "Reżyser jest wymagany")]
-    public string? Director { get; set; }
-    [DisplayName("Data Wydania")]
+    public string Director { get; set; }
+    
+    [DisplayName("Data wydania")]
     [Required(ErrorMessage = "Data wydania jest wymagana")]
     public DateOnly ReleaseDate { get; set; }
+    
     [DisplayName("Gatunek")]
     [Required(ErrorMessage = "Gatunek jest wymagany")]
-    public string? Genre { get; set; }
-    [DisplayName("Cena")]
-    [Range(0,2000,ErrorMessage = "Cena musi być w przedziale 0 do 2000")]
-    public double? Price { get; set; }
+    public string Genre { get; set; }
     
+    [DisplayName("Cena")]
+    [Required(ErrorMessage = "Cena jest wymagana")]
+    [Range(0, 2000, ErrorMessage = "Cena musi być większa od 0, 2000")]
+    public double Price { get; set; }
 }
--- cw7/Pages/AddMovie.cshtml.cs
+++ cw7/Pages/AddMovie.cshtml.cs
@@ -8,11 +8,40 @@ namespace cw7.Pages
     {
         [BindProperty]
         public Movie? MyMovie { get; set; }
+        public List<string>? Genres { get; set; }
+        public AddMovieModel()
+        {
+            Genres = new List<string> {
+                "Action", "Crime", "Drama",
+                "Fantasy","Sci-Fi","Comedy",
+                "Horror" };
+        }
         public void OnGet()
         {
+            ViewData["Genres"] = Genres;
+            ViewData["Message"] = "Dopiero wywitlamy formularz";
         }
-        public void OnPost(){
-            
+        public IActionResult OnPost()
+        {
+            if(MyMovie == null)
+            {
+                ViewData["Message"] = "Brak danych";
+                return Page();
+            }
+            ViewData["Genres"] = Genres;
+            if (ModelState.IsValid)
+            {
+                // Add movie to database
+                MoviesRepo repo = new MoviesRepo("movies.json");
+                repo.AddMovie(MyMovie);
+                ViewData["Message"] = "Film dodany";
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                ViewData["Message"] = "Niepoprawne dane";                
+            }
+            return Page();
         }
     }
 }
