3b08244b1a6129f7232f4e8bc89033efcf656c33
MrHerbat <kocirel206@gmail.com>
2024-12-03 16:16:15 +0100

    teehee

--- cw9_sqlite/Models/ProductsRepo.cs
+++ cw9_sqlite/Models/ProductsRepo.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Microsoft.Data.Sqlite;
 
 namespace cw9_sqlite.Models;
@@ -33,6 +34,46 @@ public class ProductsRepo
         }
         
         return products;
+    }
+
+    public Product SearchForProduct(int id)
+    {
+        Product product;
+        using SqliteConnection conn = new SqliteConnection(_connString);
+        SqliteCommand command = conn.CreateCommand();
+        command.CommandText = "SELECT * FROM products WHERE id = @id";
+        command.Parameters.AddWithValue("@id", id);
+        conn.Open();
+        using SqliteDataReader reader = command.ExecuteReader();
+        if (reader.Read())
+        {
+            product = new Product
+            {
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                Price = reader.GetDecimal(2),
+                Type = reader.GetString(3)
+            };
+            return product;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void AddProduct(Product product)
+    {
+        throw new NotImplementedException();
+        using SqliteConnection conn = new SqliteConnection(_connString);
+        SqliteCommand command = conn.CreateCommand();
+        command.CommandText = "INSERT INTO products(name,price,type) VALUES(@name,@price,@type)";
+        command.Parameters.AddWithValue("@name", product.Name);
+        command.Parameters.AddWithValue("@price", 
+            product.Price.ToString(CultureInfo.InvariantCulture));
+        command.Parameters.AddWithValue("@type", product.Type);
+        conn.Open();
+        command.ExecuteNonQuery();
     }
     
 }
--- cw9_sqlite/Program.cs
+++ cw9_sqlite/Program.cs
@@ -1,9 +1,45 @@
 ﻿using cw9_sqlite.Models;
 
+Product generateProduct()
+{
+    string? nazwa, typ;
+    decimal cena;
+    Console.Write("Podaj nazwę: ");
+    nazwa = Console.ReadLine();
+    Console.Write("Podaj cene: ");
+    cena = decimal.Parse(Console.ReadLine());
+    Console.Write("Podaj typ: ");
+    typ = Console.ReadLine();
+    
+
+    return new Product
+    {
+        Name = nazwa,
+        Price = cena,
+        Type = typ
+    };
+}
+
+
 var repo = new ProductsRepo();
 var products = repo.GetProducts();
+int idToSearch;
 foreach (var product in products)
 {
     Console.WriteLine($"{product.Id}\t{product.Name}\t " +
                       $"{product.Price}\t{product.Type}");
 }
+Console.WriteLine();
+Console.Write("Write id to search: ");
+idToSearch = Convert.ToInt32(Console.ReadLine());
+var findProduct = repo.SearchForProduct(idToSearch);
+if (findProduct == null)
+{
+    Console.WriteLine("No products found");
+}
+else
+{
+    Console.WriteLine($"{findProduct.Id}\t{findProduct.Name}\t{findProduct.Price}\t{findProduct.Type}");
+}
+Product productToAdd = generateProduct();
+repo.AddProduct(productToAdd);
