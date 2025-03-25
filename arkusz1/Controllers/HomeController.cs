d539d45da3b8bc085b39ddb2b5bcf2022145bd41
MrHerbat <kocirel206@gmail.com>
2025-03-25 14:44:46 +0100

    tak

---
+++ arkusz1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using makarony.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace makarony.Controllers
+{
+    public class HomeController : Controller
+    {
+        private RepoReservation _repo;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _repo = new RepoReservation(configuration);
+        }
+        
+        
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Result(Reservation reservation){
+            if (reservation == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            reservation.Place = 1;
+            if (!String.IsNullOrEmpty(reservation.Date) && 
+                !String.IsNullOrEmpty(reservation.Phone) &&
+                reservation.Count <= 0)
+            {
+                _repo.SaveToDb(reservation);
+                return View("Result");
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Result()
+        {
+            List<Reservation> reservations = _repo.GetAllReservations();
+            return View(reservations);
+        }
+
+    }
+}
---
+++ arkusz1/Models/RepoReservation.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace makarony.Models;
+
+public class RepoReservation
+{ 
+    
+    private string _connectionString;
+    public RepoReservation(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    public void SaveToDb(Reservation reservation)
+    {
+        using MySqlConnection conn = new MySqlConnection(_connectionString);
+        using MySqlCommand cmd = conn.CreateCommand();
+        cmd.CommandText = "INSERT INTO rezerwacje (nr_stolika,data_rez,liczba_osob,telefon) VALUES " +
+                          $"({reservation.Place},{reservation.Date},{reservation.Count},{reservation.Phone})";
+        conn.Open();
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    public List<Reservation> GetAllResevations()
+    {
+        List<Reservation> reservations = new List<Reservation>();
+        using MySqlConnection conn = new MySqlConnection(_connectionString);
+        using MySqlCommand cmd = conn.CreateCommand();
+        cmd.CommandText = "SELECT * FROM rezerwacje";
+        conn.Open();
+        using MySqlDataReader reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            var reservation = new Reservation();
+            reservation.Id = reader.GetInt32(0);
+            reservation.Place = reader.GetInt32(1);
+            var date = reader.GetDateTime(2);
+            reservation.Date = date.ToString("yyyy-MM-dd");
+            reservation.Count = reader.GetInt32(3);
+            reservation.Phone = reader.GetString(4);
+            reservations.Add(reservation);
+        }
+        conn.Close();
+        return reservations;
+    }
+}
